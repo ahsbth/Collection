3ae6411d24cabb84373242b0e07cd58faf1f4328
ahsbth <asraj48342@gmail.com>
2024-12-24 19:39:38 +0530

    done

---
+++ ArrayList/Add.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Add{
+    public static void main(String args[]){
+        ArrayList<Integer> arr=new ArrayList<>();
+        arr.add(12);
+        arr.add(12);
+        arr.add(12);
+        arr.add(12);
+        arr.add(12);
+        System.out.println(arr);
+    }
+}
